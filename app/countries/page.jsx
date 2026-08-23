9f91db3354350faf54f2a38bcba24534c92c528d
Muhammad <muhammadahmed8710@gmail.com>
2026-08-23 17:07:56 +0500

    Create page.jsx

---
+++ app/countries/page.jsx
@@ -0,0 +1,10 @@
+import CountryStatsPage from '../../components/CountryStatsPage.jsx';
+
+export const metadata = {
+  title: 'Country & region statistics | DevGlobe',
+  description: 'Developer count, average score, and top languages by country across DevGlobe.',
+};
+
+export default function CountriesPage() {
+  return <CountryStatsPage />;
+}
